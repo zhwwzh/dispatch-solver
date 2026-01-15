a5c3009ae16efdf152aec4dc59bd05a5d77285f1
zhwwzh <william.zhang.hust@gmail.com>
2026-01-15 10:01:49 +0800

    first commit

---
+++ src/main/java/net/mbi/wcloud/dispatch_solver/DispatchSolverApplication.java
@@ -0,0 +1,13 @@
+package net.mbi.wcloud.dispatch_solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mbi/wcloud/dispatch_solver/DispatchSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mbi.wcloud.dispatch_solver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
